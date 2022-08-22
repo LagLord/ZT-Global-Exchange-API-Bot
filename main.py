175f7947888da3d69df8893983bc5ab884c2f235
unknown <moinkhan95650@gmail.com>
2022-08-22 17:15:04 +0530

    Some bugs fix

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import hashlib
 import json
 import time
 import keyboard
-import websocket
-import rel
 import os, ssl
 import requests
 
@@ -11,6 +9,9 @@ api_key = "abcd"
 secretKey = "9000"
 
 tick = ""
+CRED = '\033[91m'
+CGREEN = '\33[32m'
+CEND = '\33[0m'
 
 
 def sign(params):
@@ -23,17 +24,34 @@ def sign(params):
     return signature
 
 
-def get_complete_param(amount, ticker, side):
-    params = {
-        'amount': amount,
-        'api_key': api_key,
-        'market': ticker + '_USDT',
-        'side': side,
-    }
+def get_complete_param(ticker, side=None, amount=None):
+    if amount is None and side is None:
+        params = {
+            'amount': amount,
+            'api_key': api_key,
+            'market': ticker + '_USDT',
+            'side': side,
+        }
+    else:
+        params = {
+            'api_key': api_key,
+        }
 
     sign_ = sign(params)
     params["sign"] = sign_
     return params
+
+
+def get_user_assets(ticker):
+    param = get_complete_param(ticker=ticker)
+    buy_url = "https://www.ztb.im/api/v1/private/user"
+    res = requests.post(buy_url, params=param)
+    response = json.loads(res.text)
+    if response["code"] == 0:
+        amount = response["result"][ticker]["available"]
+        return amount
+    else:
+        get_user_assets(ticker)
 
 
 def get_price(ticker):
@@ -41,14 +59,15 @@ def get_price(ticker):
     all_coins = json.loads(res.text)
     for coin in all_coins["ticker"]:
         if coin["symbol"] == ticker:
-            print(coin)
+            # print(coin)
             return coin["buy"]
 
 
 def ask_sell(ticker, amount, cost):
     while True:
         price = get_price(tick)
-        print(f"Current Profit: {(float(price) * amount) - cost} USD\n"
+        change = (float(price) * amount) - cost
+        print(f"Current Profit: {change} USD.\n"
               f"Would you like to sell your {amount} {ticker} coins?\nIf yes, press 'S' key for a few seconds.")
 
         if keyboard.is_pressed("s"):
@@ -56,28 +75,31 @@ def ask_sell(ticker, amount, cost):
                 param = get_complete_param(ticker=ticker, amount=amount, side=1)
                 buy_url = "https://www.ztb.im/api/v1/private/trade/market"
                 res = requests.post(buy_url, params=param)
-                response = json.load(res.text)
+                response = json.loads(res.text)
+                print(response)
                 if response["code"] == 0:
                     print("Sold Successfully!")
                     return ask_buy()
 
 
 def ask_buy(ticker=None, amount=None):
+    global tick
     if ticker is None and amount is None:
+        amount = input(f"How much would you like to buy?: ")
         ticker = input("Which coin would you like to buy?\n(Please provide the ticker eg. BTC, ETH): ").upper()
-        global tick
+
         tick = ticker + "_USDT"
         get_price(tick)
-        amount = input("How much would you like to buy?: ")
 
     param = get_complete_param(ticker=ticker, amount=amount, side=2)
     buy_url = "https://www.ztb.im/api/v1/private/trade/market"
     res = requests.post(buy_url, params=param)
     response = json.loads(res.text)
+    # print(response)
     if response["code"] == 0:
-        print("Bought Successfully!")
-        cost = float(response["result"]["price"]) * float(amount)
-        ask_sell(ticker, amount, cost)
+        print(f"Bought Successfully!")
+        cost = float(amount)
+        ask_sell(ticker, get_user_assets(ticker), cost)
 
     else:
         print("Something went wrong!\nTrying...")
@@ -85,8 +107,9 @@ def ask_buy(ticker=None, amount=None):
 
 
 # while True:
+#         tick = "AVAX_USDT"
 #         price = get_price(tick)
-#         print(f"Current Profit:scoins?\nIf yes, press 'S' key.")
+#         print(f"Current Price: {type(price)}\nIf yes, press 'S' key.")
 #
 #         if keyboard.is_pressed("s"):
 #             print("PRESSED!")
